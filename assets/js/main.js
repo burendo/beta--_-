f03360c817ed291175eb177d44cef6a3620908c2
burendo <89887686+burendo@users.noreply.github.com>
2021-09-10 11:52:48 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,3 @@
-let blackbox = document.getElementById("blackbox");
-
-const scrollEvent = function () {
-    window.addEventListener("scroll", function () {
-
-        // スクロール量の取得
-        let scrollValue = window.pageYOffset;
-
-        blackbox.style.opacity = scrollValue / 800;
-
-        if (scrollValue > 1000) {blackbox.style.opacity = 0;}
-    });
-};
-
-scrollEvent();
-
 const StageApp = Vue.createApp({
   data() {
     return {
