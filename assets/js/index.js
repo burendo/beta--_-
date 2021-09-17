5cfd6ed339d20151f4f7e23fdf8a639232668350
burendo <89887686+burendo@users.noreply.github.com>
2021-09-17 09:29:23 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,9 @@ const scrollEvent = function () {
         // スクロール量の取得
         let scrollValue = window.pageYOffset;
 
-        blackbox.style.opacity = scrollValue / 800;
+        blackbox.style.opacity = scrollValue / 500;
 
-        // if (scrollValue > 1000) {blackbox.style.opacity = 0;}
+        if (scrollValue > 1000) {blackbox.style.opacity = 0;}
     });
 };
 
