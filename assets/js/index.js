7d4f3a3164b05539ebe674ccbc9ce0fd6a8d314d
burendo <89887686+burendo@users.noreply.github.com>
2021-09-17 09:39:38 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,6 @@ let blackbox = document.getElementById("blackbox");
 
 const scrollEvent = function () {
     window.addEventListener("scroll", function () {
-
         // スクロール量の取得
         let scrollValue = window.pageYOffset;
 
